d2a0beb5ba0c6cf07daa49416504544f28c79059
Uri Kreindler <uri.kreindler@berkeley.edu>
2022-01-09 23:22:30 -0800

    header

--- gui.py
+++ gui.py
@@ -4,6 +4,8 @@ from Engine import *
 WIDTH = HEIGHT = 512
 DIMENSION = 8
 SQ_SIZE = HEIGHT / DIMENSION
+MOVE_LOG_WIDTH = 280
+MOVE_LOG_HEIGHT = HEIGHT
 MAX_FPS = 15
 IMAGES = {}
 COLORS = [p.Color("white"), p.Color("tan")]
@@ -16,8 +18,10 @@ def load_images():
 
 def run(fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
     p.init()
-    screen = p.display.set_mode((WIDTH, HEIGHT))
+    screen = p.display.set_mode((WIDTH + MOVE_LOG_WIDTH, HEIGHT))
     clock = p.time.Clock()
+    move_log_font = p.font.SysFont("Helvitca", 16, True, False)
+    heading_font = p.font.SysFont("Helvitca", 32, True, False)
     screen.fill(p.Color("white"))
     gs = Engine(fen)
     load_images()
@@ -32,21 +36,24 @@ def run(fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
                 location = p.mouse.get_pos() # (X,Y) location of mouse cursor
                 col = location[0] // SQ_SIZE
                 row = location[1] // SQ_SIZE
-                if sq_selected == (row, col): # Clicked same square twice
+                if sq_selected == (row, col) or col >= 8: # Clicked same square twice or clicked move log
                     sq_selected = () # Deselect
                     player_clicks = [] # Clear
                 else:
                     sq_selected = (row, col)
                     player_clicks.append(sq_selected)
                 if len(player_clicks) == 2: # After second click
-                    move = (translate(player_clicks[0]), translate(player_clicks[1]))
-                    print(move)
-                    gs.gui_move(move)
-                    sq_selected = () # Reset
-                    player_clicks = [] # Reset
+                    if gs.official_board.BOARD[translate(player_clicks[0])] != "-" and gs.official_board.BOARD[translate(player_clicks[1])] != "-" and gs.official_board.BOARD[translate(player_clicks[0])].color == gs.official_board.BOARD[translate(player_clicks[1])].color:
+                        sq_selected = player_clicks[1]
+                        player_clicks = [sq_selected]
+                    else:
+                        move = (translate(player_clicks[0]), translate(player_clicks[1]))
+                        gs.gui_move(move)
+                        sq_selected = () # Reset
+                        player_clicks = [] # Reset
 
 
-        draw_game_state(screen, gs, sq_selected)
+        draw_game_state(screen, gs, sq_selected, move_log_font, heading_font)
         clock.tick(MAX_FPS)
         p.display.flip() 
 
@@ -72,13 +79,13 @@ def highlight_squares(screen, gs, sq_selected):
             for move_tuple in possible_moves:
                 if move_tuple[1] == translate(sq_selected):
                     move = untranslate(move_tuple[2])
-                    print(move)
                     screen.blit(s, (move[1] * SQ_SIZE, move[0] * SQ_SIZE))
 
-def draw_game_state(screen, gs, sq_selected):
+def draw_game_state(screen, gs, sq_selected, move_log_font, heading_font):
     draw_board(screen)
     highlight_squares(screen, gs, sq_selected)
     draw_pieces(screen, gs.official_board)
+    draw_move_log(screen, gs, move_log_font, heading_font)
 
 def draw_board(screen):
     for r in range(DIMENSION):
@@ -92,6 +99,37 @@ def draw_pieces(screen, board):
             piece = board.BOARD[c + (7-r) * 8]
             if piece != "-":
                 screen.blit(IMAGES[piece.gui_id], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def draw_move_log(screen, gs, font, heading_font):
+    heading_log_rect = p.Rect(WIDTH, 0, MOVE_LOG_WIDTH, 30)
+    p.draw.rect(screen, p.Color('white'), heading_log_rect)
+    text_obj = heading_font.render("Tilapia 1.0", True, p.Color('black'))
+    text_loc = heading_log_rect.move(5, 5)
+    screen.blit(text_obj, text_loc)
+    screen.blit(p.image.load("images/tilapia_2_60x30.png"), p.Rect(WIDTH + 150, 0, 60, 30))
+    move_log_rect = p.Rect(WIDTH, 30, MOVE_LOG_WIDTH, MOVE_LOG_HEIGHT)
+    p.draw.rect(screen, p.Color('black'), move_log_rect)
+    move_log = gs.move_log
+    move_texts = []
+    for i in range(0, len(move_log), 2):
+        move_str = str(i // 2 + 1) + ". " + move_log[i] + " "
+        if i + 1 < len(move_log):
+            move_str += move_log[i + 1] + " "
+        move_texts.append(move_str)
+
+    moves_per_row = 3
+    padding = 5
+    line_spacing = 2
+    textY = padding
+    for i in range(0, len(move_texts), moves_per_row):
+        text = ""
+        for j in range(moves_per_row):
+            if i + j < len(move_texts):
+                text += move_texts[i + j]
+        text_obj = font.render(text, True, p.Color('white'))
+        text_loc = move_log_rect.move(padding, textY)
+        screen.blit(text_obj, text_loc)
+        textY += text_obj.get_height() + line_spacing
 
 run()
 
